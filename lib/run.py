9b610a4248f375a7782068b46acf1376d6d4bd29
Nikita Petrenko <npetrenko97@gmail.com>
2020-11-27 02:43:47 +0300

    whitespace fix

--- lib/run.py
+++ lib/run.py
@@ -7,6 +7,5 @@ def run(args, **kwargs):
     print_log("+ '" + ' '.join(args) + "'")
     return subprocess.run(args, check=True, **kwargs)
 
-
 def sudo_run(args, **kwargs):
     return run(['sudo', '-E'] + args, **kwargs)
